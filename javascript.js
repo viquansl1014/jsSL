616fe094624fc79c4632c6b72e92062385c38c26
=v <viquansl1014@gmail.com>
2020-10-19 16:44:24 -0500

    1b

--- javascript.js
+++ javascript.js
@@ -4,3 +4,29 @@ var num1= 100;
 var num2= 200;
 
  console.log(num1 +_num2);
+ console.log(typeof num1);
+
+ var number1 = new Number(1000);
+ var number2 = new Number(1000);
+ var number3 = number3;
+
+ console.log(number1 == number2);
+
+ console.log("say 'a'");
+ console.log('say "a"');
+ console.log('say \'a\'');
+
+ var value1 = null;
+ var value2;
+
+ if (value1){
+     console.log("good");
+ }else{
+     console.log("bad");
+ }
+
+ if (value2){
+    console.log("good");
+}else{
+    console.log("bad");
+}
---
+++ jsBeginner.js
@@ -0,0 +1,13 @@
+var string1 = new String('New String');
+var string2 = new String('New String');
+var string3 = 'New String';
+var string4 = string1;
+
+
+ console.log(string1 == string2);
+ console.log(string1 == string3);
+ console.log(string1 == string4);
+ console.log(string1 === string4);
+
+ console.log(typeof string1);
+ console.log(typeof string3);
